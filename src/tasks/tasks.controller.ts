2ee6952ece1fc0740a636b0a66806d709ba17b83
mr-chidex <mbahchiboy2@gmail.com>
2023-01-06 20:37:49 +0100

    crud endpoint with dumm data

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAll();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDTO) {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get(':id')
+  getTask(@Param('id') taskId: string) {
+    return this.tasksService.geTask(taskId);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAll(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDTO): Task {
+    const { title, description } = createTaskDto;
+
+    const newTask: Task = {
+      id: Date.now().toString(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(newTask);
+    return newTask;
+  }
+
+  geTask(taskId: string) {
+    return this.findOne(taskId);
+  }
+
+  findOne(id: string) {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) throw new NotFoundError('task not found');
+
+    return task;
+  }
+}
