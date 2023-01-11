adde52893e046048a6cf36964ccdf8d189e67024
mr-chidex <mbahchiboy2@gmail.com>
2023-01-11 18:38:38 +0100

    auth guard for task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,9 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { TaskFilterDto } from './dto/task-filter.dto';
 import { TaskStatusPipe } from './pipes/task-status-validation.pipe';
@@ -18,6 +20,7 @@ import { Task, TaskStatus } from './tasks.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
