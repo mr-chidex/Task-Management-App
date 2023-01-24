93883473d96bbc6e0e69e15707380fa039665efe
mr-chidex <mbahchiboy2@gmail.com>
2023-01-24 11:08:20 +0100

    clean

--- src/auth/pipes/auth.pipe.ts
+++ src/auth/pipes/auth.pipe.ts
@@ -8,7 +8,7 @@ import {
 @Injectable()
 export class AuthPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    // make sure username is saved as lowerase
+    // make sure username is saved as lowercase
     if (value.username) {
       value.username = value.username?.toLowerCase();
     }
