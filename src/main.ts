a7ce8c5fb87b13c62ac6f46a25c9c84d0ddd7fe0
mr-chidex <mbahchiboy2@gmail.com>
2023-01-26 12:06:30 +0100

    adding logger for error and info logging

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
 
-  await app.listen(3000);
+  const PORT = 3000;
+
+  await app.listen(PORT);
+  logger.log(`Application running on PORT::>> ${PORT}`);
 }
 bootstrap();
