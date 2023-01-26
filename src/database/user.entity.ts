03b066ab0432b453042d738a8eb2c5f5b6dbfa52
mr-chidex <mbahchiboy2@gmail.com>
2023-01-26 10:26:42 +0100

    comment added

--- src/database/user.entity.ts
+++ src/database/user.entity.ts
@@ -20,6 +20,8 @@ export class User extends BaseEntity {
   @Column()
   password: string;
 
+  //eager set to true: whenever we retrieve the user we can access user.tasks immediately, and
+  //get an array of tasks owned by the user. Only one side can have the eager true prop
   @OneToMany((task) => Task, (task) => task.user, { eager: true })
   tasks: Task[];
 }
